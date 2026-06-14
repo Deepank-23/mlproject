7320b7c718f01e636643b438421236c7830d91bf
Deepank-23 <deepankagarwal23@gmail.com>
2026-06-14 19:04:11 +0530

    data ingestion

--- diagnose_matplotlib_import.py
+++
@@ -1,8 +0,0 @@
-import traceback
-try:
-    import matplotlib
-    print('matplotlib.__file__:', matplotlib.__file__)
-    import matplotlib.pyplot as plt
-    print('pyplot OK')
-except Exception:
-    traceback.print_exc()
